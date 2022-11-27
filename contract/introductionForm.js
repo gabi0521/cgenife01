9e53f3e78396eb704045f3cc3a9657ca8841b7d3
gabi0521 <99023000+gabi0521@users.noreply.github.com>
2022-11-27 13:14:02 -0500

    edited

---
+++ contract/introductionForm.js
@@ -0,0 +1,23 @@
+
+   
+function changehead(){
+    var fName = document.getElementById("fName").value;
+    var mName = document.getElementById("mName").value;
+    var lName = document.getElementById("lName").value;
+    document.getElementById("display").innerHTML = "Welcome to Shebba Mineral Introduction Form Page," +fName+" "+mName+" "+lName;
+
+}
+function displayEntry(){
+    var Question1 = document.getElementById("qNum1").value;
+    var Question2 = document.getElementById("qNum2").value;
+    var Question3 = document.getElementById("qNum3").value;
+    var Question4 = document.getElementById("qNum4").value;
+    var Question5 = document.getElementById("qNum5").value;
+    var Question6 = document.getElementById("qNum6").value;
+    document.getElementById("showAnswers1").innerHTML = "Personal background:"+ Question1; 
+    document.getElementById("showAnswers2").innerHTML = "professional background:"+ Question2;
+    document.getElementById("showAnswers3").innerHTML = "Academic background:"+ Question3;
+    document.getElementById("showAnswers4").innerHTML = "Information/technology background:"+ Question4;
+    document.getElementById("showAnswers5").innerHTML = "primary computer platform:"+ Question5;
+    document.getElementById("showAnswers6").innerHTML = "Anything else you would like to share:"+ Question6 ;
+}       
